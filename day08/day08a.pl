6cd501b040ae19cf88d1f544e40511b43c9616be
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-12-10 00:57:26 -0500

    Add day 8, first part

---
+++ day08/day08a.pl
@@ -0,0 +1,25 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+use 5.022;
+
+my $ifname = 'day08_input';
+open my $fh, '<', $ifname or die "Can't open $ifname: $!";
+
+my $total;
+
+while (<$fh>) {
+    chomp;
+
+    my $len = length $_;
+
+    s/^"(.*)"$/$1/;
+    s/\\\\/S/g;
+    s/\\"/Q/g;
+    s/\\x[[:xdigit:]]{2}/X/g;
+
+    $total += $len - length $_;
+}
+
+say "Total diff: $total";
