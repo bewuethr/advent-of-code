74335d604706e89586191c7608544d57fb4fc897
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-12-19 23:33:06 -0500

    Add day 18, first part (Perl)

---
+++ day18/day18a.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use 5.022;
+use List::Util qw(sum);
+
+sub get_neighbours {
+    my ($x, $y, $grid) = @_;
+    my $count = 0;
+    foreach my $yy ($y-1 .. $y+1) {
+        next if $yy < 0 or $yy > $#{$grid};
+        foreach my $xx ($x-1 .. $x+1) {
+            next if $xx < 0 or $xx > $#{$grid->[$y]} or $xx == $x and $yy == $y;
+            ++$count if $grid->[$yy][$xx] eq '#';
+        }
+    }
+    return $count;
+}
+
+my @grid;
+open my $fh, '<', 'input' or die "Can't open input file: $!";
+while (<$fh>) {
+    chomp;
+    push @grid, [ split // ];
+}
+
+
+foreach (1..100) {
+    my @ncount;
+    foreach my $y (0..$#grid) {
+        foreach my $x (0 .. $#{$grid[$y]}) {
+            push @{$ncount[$y]}, get_neighbours($x, $y, \@grid);
+        }
+    }
+
+    foreach my $y (0..$#grid) {
+        foreach my $x (0 .. $#{$grid[$y]}) {
+            if ($grid[$y][$x] eq '#') {
+                if ($ncount[$y][$x] != 2 and $ncount[$y][$x] != 3) {
+                    $grid[$y][$x] = '.';
+                }
+            }
+            else {
+                $grid[$y][$x] = '#' if $ncount[$y][$x] == 3;
+            }
+        }
+    }
+}
+
+my $lights_on = sum map { scalar grep { $_ eq '#' } @{$_} } @grid;
+say "There are $lights_on lights on.";
