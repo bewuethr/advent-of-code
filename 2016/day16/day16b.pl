02ca1be2f53a7bdf7f99bce73e6c5da749b456e8
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2016-12-16 01:20:44 -0500

    Beautify Perl solution

--- 2016/day16/day16b.pl
+++ 2016/day16/day16b.pl
@@ -13,10 +13,13 @@ chomp $str;
 
 my $len = 35651584;
 
-$str .= "0" . scalar reverse ($str =~ tr/01/10/r) while ((length $str) < $len);
+$str .= 0 . reverse ($str =~ tr/01/10/r) while length($str) < $len;
 
 my $chksum = substr $str, 0, $len;
 
-$chksum = join "", map { (substr $_, 0, 1) == (substr $_, 1) ? "1" : "0" } ( $chksum =~ m/../g ) while ((length $chksum) %2 == 0); 
+$chksum
+    = join "",
+      map { (substr $_, 0, 1) == (substr $_, 1) ? 1 : 0 } ($chksum =~ m/../g)
+      until length($chksum) % 2;
 
 say $chksum;
