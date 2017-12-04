c10dc5e1a520c5de6dc154f1b81109335ea88217
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2017-12-04 00:27:05 -0500

    Day 04, add Perl for part one, clean up

--- 2017/day04/day04a.pl
+++ 2017/day04/day04a.pl
@@ -14,8 +14,7 @@ my $ctr = 0;
 
 while (my $line = <$fh>) {
     chomp $line;
-    my @arr = map { my @chars = split //, $_; join '', sort @chars } split / /, $line;
-    $ctr++ if not ( (join ' ', @arr) =~ /(\b\w+\b).*\1/ );
+    $ctr++ if not $line =~ /(\b\w+\b).*\1/;
 }
 
 say $ctr;
