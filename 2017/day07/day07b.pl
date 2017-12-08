a91d336f6c62f8e8dced6da6b1f3b82394837a32
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2017-12-08 01:34:05 -0500

    Make part 2 of day 07 actually find something

--- 2017/day07/day07b.pl
+++ 2017/day07/day07b.pl
@@ -5,54 +5,60 @@ use warnings;
 
 use feature 'say';
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(mesh uniq frequency firstidx lastidx);
-use List::Util qw(max min product sum);
-use Math::Prime::Util qw(fordivisors);
+use List::MoreUtils qw(firstval singleton);
+use List::Util qw(sum);
 
 sub getweight {
     my ($node, $data) = @_;
-    my $debug = 0;
-    say "looking at $node" if $debug;
     my $weight = $data->{$node}{weight};
+
     return $weight if not exists $data->{$node}{children};
-    say "$node has children" if $debug;
 
     foreach my $child (keys %{ $data->{$node}{children} }) {
-        say "getting weight for $child" if $debug;
         $data->{$node}{children}{$child} = getweight($child, $data);
-        say "weight for $child is $data->{$node}{children}{$child}" if $debug;
     }
-    print "Children of $node: " . Dumper($data->{$node}{children}) if $debug;
     return $weight + sum values %{ $data->{$node}{children} };
 }
-
 
 my $fname = shift;
 
 open my $fh, "<", $fname
     or die "Can't open $fname: $!";
 
-my $res = 0;
+chomp(my $root = `./day07a input`);
 
-my $root = 'dgoocsw';
-# my $root = 'tknk';
 my %data;
 
+# Build data structure
 while (my $line = <$fh>) {
     chomp $line;
     my @arr = split /[- (),>]+/, $line;
-    $data{$arr[0]}{weight} = $arr[1];
-    $data{$arr[0]}{children} = { map { $_ => 0 }  @arr[2..$#arr] } if $#arr > 1;
+
+    $data{$arr[0]}{weight  } = $arr[1];
+    $data{$arr[0]}{children} = { map { $_ => 0 } @arr[2..$#arr] } if $#arr > 1;
 }
 
+# Get aggregated weights
 my $node = $root;
 getweight($node, \%data);
 
-print Dumper(\%data);
+my @imbalanced;
 
-say $res; # Now inspect me manually!
+# Find program causing imbalance
+while (1) {
+    push @imbalanced, {
+        weight   => $data{$node}{weight},
+        children => [ values %{$data{$node}{children}} ]
+    };
+    my $children = $data{$node}{children};
+    my ($bad_weight) = singleton values %$children;
+    last if not defined $bad_weight;
+    $imbalanced[-1]{bad} = $bad_weight;
+    my ($bad_child) = grep { $children->{$_} == $bad_weight } keys %$children;
+    $node = $bad_child;
+}
+
+my $diff 
+    = $imbalanced[-2]{bad}
+      - firstval { $_ != $imbalanced[-2]{bad} } @{$imbalanced[-2]{children}};
+say $imbalanced[-1]{weight} - $diff;
