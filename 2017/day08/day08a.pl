80cbe30120c04e4e5d73fa526d52c7987509a0b2
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2017-12-08 00:15:25 -0500

    Add day 08, Perl, as submitted

---
+++ 2017/day08/day08a.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use feature 'say';
+
+use Algorithm::Combinatorics qw(permutations combinations variations);
+use Data::Dumper;
+use Digest::MD5 qw(md5_hex);
+use File::Slurp;
+use Graph::Simple;
+use List::MoreUtils qw(mesh uniq frequency firstidx lastidx singleton);
+use List::Util qw(max min product sum);
+use Math::Prime::Util qw(fordivisors);
+
+my $fname = shift;
+
+open my $fh, "<", $fname
+    or die "Can't open $fname: $!";
+
+my %vars;
+
+while (my $line = <$fh>) {
+    chomp $line;
+    my @arr = split / /, $line;
+    foreach my $idx (0, 4) {
+        $vars{$arr[$idx]} //= 0;
+        $arr[$idx] =~ s/.*/\$vars{$&}/;
+    }
+    $arr[1] =~ s/inc/+=/;
+    $arr[1] =~ s/dec/-=/;
+    eval join ' ', @arr;
+}
+
+say max values %vars;
