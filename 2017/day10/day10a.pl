8fc1859a5a32342d857d44c9a2d3038685f1748e
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2017-12-10 01:49:32 -0500

    Clean up Perl for day 10

--- 2017/day10/day10a.pl
+++ 2017/day10/day10a.pl
@@ -5,15 +5,6 @@ use warnings;
 
 use feature 'say';
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(firstval mesh uniq frequency firstidx lastidx singleton);
-use List::Util qw(max min product sum);
-use Math::Prime::Util qw(fordivisors);
-
 my $fname = shift;
 
 open my $fh, "<", $fname
@@ -27,17 +18,11 @@ my $skip = 0;
 my $cur = 0;
 
 my @list = (0..255);
-# my @list = (0..4);
 my @list2 = @list;
 
-my $debug = 1;
-
 foreach my $len (@lengths) {
-    say '============' if $debug;
-    say "cur: $cur, len: $len, skip: $skip" if $debug;
-    say "@list (list)" if $debug;
     my @longlist = (@list, @list2);
-    say "@longlist (longlist)" if $debug;
+
     my @sublist;
     if ($len == 0) {
         @sublist = ();
@@ -45,7 +30,7 @@ foreach my $len (@lengths) {
     else {
         @sublist = @longlist[$cur..$cur+$len-1];
     }
-    print "sublist: " . Dumper(\@sublist) if $debug;
+
     my @head;
     if ($cur == 0) {
         @head = ();
@@ -53,19 +38,18 @@ foreach my $len (@lengths) {
     else {
         @head = @longlist[0..$cur-1];
     }
-    print "head " . Dumper(\@head) if $debug;
+
     my @mod_longlist = (@head, (reverse @sublist), @longlist[ $cur+$len .. $#longlist]);
-    print "mod_longlist " . Dumper(\@mod_longlist) if $debug;
+
     @list = @mod_longlist[0..$#list];
     if ($cur+$len-1 > $#list) {
         @list[ 0 .. ($cur+$len-1) % @list ] = @mod_longlist[ scalar @list .. $cur+$len-1 ];
     }
     @list2 = @list;
+
     $cur += $len + $skip;
     $cur %= @list;
     $skip++;
-    say "@list (list at end)" if $debug;
 }
 
 say $list[0] * $list[1];
-
--- 2017/day10/day10b.pl
+++ 2017/day10/day10b.pl
@@ -5,14 +5,7 @@ use warnings;
 
 use feature 'say';
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(firstval mesh uniq frequency firstidx lastidx singleton);
-use List::Util qw(reduce max min product sum);
-use Math::Prime::Util qw(fordivisors);
+use List::Util qw(reduce);
 
 my $fname = shift;
 
@@ -29,18 +22,11 @@ my $skip = 0;
 my $cur = 0;
 
 my @list = (0..255);
-# my @list = (0..4);
 my @list2 = @list;
-
-my $debug = 0;
 
 foreach my $i (1..64) {
     foreach my $len (@lengths) {
-        say '============' if $debug;
-        say "cur: $cur, len: $len, skip: $skip" if $debug;
-        say "@list (list)" if $debug;
         my @longlist = (@list, @list2);
-        say "@longlist (longlist)" if $debug;
         my @sublist;
         if ($len == 0) {
             @sublist = ();
@@ -48,7 +34,6 @@ foreach my $i (1..64) {
         else {
             @sublist = @longlist[$cur..$cur+$len-1];
         }
-        print "sublist: " . Dumper(\@sublist) if $debug;
         my @head;
         if ($cur == 0) {
             @head = ();
@@ -56,9 +41,7 @@ foreach my $i (1..64) {
         else {
             @head = @longlist[0..$cur-1];
         }
-        print "head " . Dumper(\@head) if $debug;
         my @mod_longlist = (@head, (reverse @sublist), @longlist[ $cur+$len .. $#longlist]);
-        print "mod_longlist " . Dumper(\@mod_longlist) if $debug;
         @list = @mod_longlist[0..$#list];
         if ($cur+$len-1 > $#list) {
             @list[ 0 .. ($cur+$len-1) % @list ] = @mod_longlist[ scalar @list .. $cur+$len-1 ];
@@ -67,17 +50,8 @@ foreach my $i (1..64) {
         $cur += $len + $skip;
         $cur %= @list;
         $skip++;
-        say "@list (list at end)" if $debug;
     }
 }
 
-say "@list";
-
-say $#list;
-
 my @densehash = map { reduce { $a ^ $b } @list[$_*16..($_+1)*16-1] } (0..15);
-print Dumper(\@densehash);
-
-say "@densehash";
-
 say join '', map { sprintf '%02x', $_ } @densehash;
