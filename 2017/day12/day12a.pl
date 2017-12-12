9729fa81270841e9f281e9d6fbd6821be9cd242e
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2017-12-12 00:28:09 -0500

    Clean up day 12

--- 2017/day12/day12a.pl
+++ 2017/day12/day12a.pl
@@ -5,15 +5,6 @@ use warnings;
 
 use feature 'say';
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(firstval mesh uniq frequency firstidx lastidx singleton);
-use List::Util qw(reduce max min product sum);
-use Math::Prime::Util qw(fordivisors);
-
 sub visit {
     my ($con, $seen, $el) = @_;
 
@@ -30,11 +21,6 @@ my $fname = shift;
 open my $fh, "<", $fname
     or die "Can't open $fname: $!";
 
-# chomp(my @arr = <$fh>);
-# 
-# while (1) {
-# }
-
 my %con;
 
 while (my $line = <$fh>) {
@@ -42,8 +28,6 @@ while (my $line = <$fh>) {
     my @arr = split /[ <>,-]+/, $line;
     $con{$arr[0]} = [ @arr[1 .. $#arr] ];
 }
-
-# print Dumper(\%con);
 
 my $el = 0;
 my %seen = ( $el => 1 );
--- 2017/day12/day12b.pl
+++ 2017/day12/day12b.pl
@@ -5,14 +5,7 @@ use warnings;
 
 use feature 'say';
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(firstval mesh uniq frequency firstidx lastidx singleton);
-use List::Util qw(reduce max min product sum);
-use Math::Prime::Util qw(fordivisors);
+use List::MoreUtils qw(firstval);
 
 sub visit {
     my ($con, $seen, $el) = @_;
@@ -30,24 +23,21 @@ my $fname = shift;
 open my $fh, "<", $fname
     or die "Can't open $fname: $!";
 
-# chomp(my @arr = <$fh>);
-# 
-# while (1) {
-# }
-
 my %con;
+my $progs = 0;
 
 while (my $line = <$fh>) {
     chomp $line;
     my @arr = split /[ <>,-]+/, $line;
     $con{$arr[0]} = [ @arr[1 .. $#arr] ];
+    $progs++;
 }
 
 my %seen;
 my $groups = 0;
 
-while (scalar keys %seen < 2000) {
-    my $el = firstval { not $seen{$_} } (0..1999);
+while (scalar keys %seen < $progs) {
+    my $el = firstval { not $seen{$_} } ( 0 .. $progs-1 );
     $seen{$el}++;
     visit(\%con, \%seen, $el);
     $groups++;
