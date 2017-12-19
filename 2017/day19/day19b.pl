ea74b3c9ad79a4598ee50b84741821df5c8679f0
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2017-12-19 01:11:02 -0500

    Clean up day 19, Perl

--- 2017/day19/day19b.pl
+++ 2017/day19/day19b.pl
@@ -5,39 +5,21 @@ use warnings;
 
 use feature 'say';
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(firstval mesh uniq frequency firstidx lastidx singleton);
-use List::Util qw(reduce max min product sum);
-use Math::Prime::Util qw(fordivisors);
-
-our $debug = 1;
+use List::MoreUtils qw(firstidx);
 
 sub walk {
     my ($x, $y, $dx, $dy, $map, $steps) = @_;
-    say "at $x/$y, direction $dx/$dy, over $map->[$y][$x]" if $debug;
     do {
-        $$steps += 1;
-        say $$steps;
+        $$steps++;
         $x += $dx;
         $y += $dy;
-        if ($map->[$y][$x] =~ /[A-Z]/) {
-            say "Found $map->[$y][$x]" if $debug; 
-            if ($map->[$y][$x] eq "S") {
-                say $$steps;
-                exit;
-            }
-        }
-    } until ($map->[$y][$x] eq "+");
+    } until ($map->[$y][$x] =~ /[+ ]/ or not defined $map->[$y][$x]);
+    $$steps-- if $map->[$y][$x] eq " " or not defined $map->[$y][$x];
     return ($x, $y);
 }
 
 sub finddir {
     my ($x, $y, $dx, $dy, $map) = @_;
-    say "finddir: at $x/$y, direction $$dx/$$dy, over $map->[$y][$x]" if $debug;
     my ($dx_next, $dy_next);
     foreach my $dir ([0, 1], [1, 0], [0, -1], [-1, 0]) {
         next if $dir->[0] == $$dx or $dir->[1] == $$dy;
@@ -64,8 +46,6 @@ while (my $line = <$fh>) {
     push @map, \@row;
 }
 
-# print Dumper(\@map);
-
 my $y = 0;
 my $x = firstidx { $_ eq "|" } @{$map[0]};
 
@@ -76,3 +56,5 @@ while (1) {
     ($x, $y) = walk($x, $y, $dx, $dy, \@map, \$steps);
     last if not finddir($x, $y, \$dx, \$dy, \@map);
 }
+
+say $steps;
