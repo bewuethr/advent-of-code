debbb7820ef53c1b8a0ef69f97e8391fe13b64b6
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2017-12-24 02:31:19 -0500

    Clean up Perl, day 24

--- 2017/day24/day24a.pl
+++ 2017/day24/day24a.pl
@@ -5,33 +5,18 @@ use warnings;
 
 use feature 'say';
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
-$Data::Dumper::Indent = 0;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(firstval mesh uniq frequency firstidx lastidx pairwise singleton);
-use List::Util qw(reduce max min product sum);
-use Math::Prime::Util qw(fordivisors);
-
-our $debug = 0;
+use List::Util qw(sum max);
 
 sub findstrongest {
     my ($first, $comps) = @_;
-    say "Called with head $first, elements: ", Dumper($comps) if $debug;
     my $strongest = 0;
     foreach my $idx (0 .. $#$comps) {
-        if ($comps->[$idx][0] == $first or $comps->[$idx][1] == $first) {
-            say "Checking subbridge starting with ", Dumper($comps->[$idx]) if $debug;
+        if ( grep { $_ == $first } @{ $comps->[$idx] } ) {
             my $newfirst = $comps->[$idx][0] == $first ? $comps->[$idx][1] : $comps->[$idx][0];
             my @newcomps = @$comps;
             splice @newcomps, $idx, 1;
-            my $strength = sum @{ $comps->[$idx] };
-            $strength += findstrongest($newfirst, \@newcomps);
+            my $strength = ( sum @{ $comps->[$idx] } ) + findstrongest($newfirst, \@newcomps);
             $strongest = max ($strongest, $strength);
-            say "Current strongest: $strongest" if $debug;
         }
     }
     return $strongest;
@@ -43,13 +28,10 @@ open my $fh, "<", $fname
     or die "Can't open $fname: $!";
 
 chomp(my @comps = <$fh>);
-my @starts;
 
 foreach my $idx (0 .. $#comps) {
-    $comps[$idx] = [ sort { $a+0 <=> $b+0 } split /\//, $comps[$idx] ];
+    $comps[$idx] = [ split /\//, $comps[$idx] ];
 }
-
-# print Dumper(\@comps);
 
 my $strongest = findstrongest(0, \@comps);
 
--- 2017/day24/day24b.pl
+++ 2017/day24/day24b.pl
@@ -5,38 +5,27 @@ use warnings;
 
 use feature 'say';
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
-$Data::Dumper::Indent = 0;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(firstval mesh uniq frequency firstidx lastidx pairwise singleton);
-use List::Util qw(reduce max min product sum);
-use Math::Prime::Util qw(fordivisors);
-
-our $debug = 0;
+use List::Util qw(sum);
 
 sub findlongest {
     my ($first, $comps) = @_;
-    say "Called with head $first, elements: ", Dumper($comps) if $debug;
     my %longestprops = (
         strength => 0,
         length => 0,
     );
 
     foreach my $idx (0 .. $#$comps) {
-        if ($comps->[$idx][0] == $first or $comps->[$idx][1] == $first) {
-            say "Checking subbridge starting with ", Dumper($comps->[$idx]) if $debug;
+        if ( grep { $_ == $first } @{ $comps->[$idx] } ) {
             my $newfirst = $comps->[$idx][0] == $first ? $comps->[$idx][1] : $comps->[$idx][0];
             my @newcomps = @$comps;
             splice @newcomps, $idx, 1;
             my $strength = sum @{ $comps->[$idx] };
-            my $length = 1;
+            my $length   = 1;
             my $subprops = findlongest($newfirst, \@newcomps);
+
             $strength += $subprops->{strength};
-            $length += $subprops->{length};
+            $length   += $subprops->{length};
+
             if ($length > $longestprops{length} or
                 $length == $longestprops{length} and $strength > $longestprops{strength})
             {
@@ -56,10 +45,8 @@ chomp(my @comps = <$fh>);
 my @starts;
 
 foreach my $idx (0 .. $#comps) {
-    $comps[$idx] = [ sort { $a+0 <=> $b+0 } split /\//, $comps[$idx] ];
+    $comps[$idx] = [ split /\//, $comps[$idx] ];
 }
-
-# print Dumper(\@comps);
 
 my $longest = findlongest(0, \@comps);
 
