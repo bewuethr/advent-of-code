3aeba8ba2618a6bb11fcd9af6629ba3544d26405
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2017-12-24 22:20:24 -0500

    Clean up Perl, day 23

--- 2017/day23/day23a.pl
+++ 2017/day23/day23a.pl
@@ -6,17 +6,7 @@ no warnings 'experimental';
 
 use v5.10.1;
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(firstval mesh uniq frequency firstidx lastidx pairwise singleton);
-use List::Util qw(reduce max min product sum);
-use Math::Prime::Util qw(fordivisors);
-
 my $fname = shift;
-my $debug = shift;
 
 open my $fh, "<", $fname
     or die "Can't open $fname: $!";
@@ -38,7 +28,6 @@ my %regs = (
 
 while (1) {
     my @instr = split / /, $arr[$idx];
-    say $idx+1, ": @instr" if $debug;
     given ($instr[0]) {
         when (/set/) {
             $regs{$instr[1]} = $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
@@ -48,18 +37,10 @@ while (1) {
             $regs{$instr[1]} -= $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
             $idx++;
         }
-        when (/add/) {
-            $regs{$instr[1]} += $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
-            $idx++;
-        }
         when (/mul/) {
             $regs{$instr[1]} *= $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
             $idx++;
             $ctr++;
-        }
-        when (/mod/) {
-            $regs{$instr[1]} %= $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
-            $idx++;
         }
         when (/jnz/) {
             if ( ( $instr[1] =~ /\d/ ? $instr[1] : $regs{$instr[1]} ) != 0) {
@@ -69,10 +50,6 @@ while (1) {
                 $idx++;
             }
         }
-    }
-    if ($debug) {
-        say "a\tb\tc\td\te\tf\tg\th";
-        say join "\t", @regs{qw/a b c d e f g h/}, "\n====";
     }
     last if ($idx < 0 or $idx > $#arr);
 }
--- 2017/day23/day23b.pl
+++ 2017/day23/day23b.pl
@@ -2,18 +2,10 @@
 
 use strict;
 use warnings;
-no warnings 'experimental';
 
-use v5.10.1;
+use feature 'say';
 
-use Algorithm::Combinatorics qw(permutations combinations variations);
-use Data::Dumper;
-use Digest::MD5 qw(md5_hex);
-use File::Slurp;
-use Graph::Simple;
-use List::MoreUtils qw(firstval mesh uniq frequency firstidx lastidx pairwise singleton);
-use List::Util qw(reduce max min product sum);
-use Math::Prime::Util qw(is_prime fordivisors);
+use Math::Prime::Util qw(is_prime);
 
 my $fname = shift;
 my $debug = shift;
@@ -23,66 +15,16 @@ open my $fh, "<", $fname
 
 chomp(my @arr = <$fh>);
 
-my $idx = 0;
-my %regs = (
-    a => 1,
-    b => 0,
-    c => 0,
-    d => 0,
-    e => 0,
-    f => 0,
-    g => 0,
-    h => 0,
-);
+@arr = map { [ split / /, $_ ] } @arr;
+
+my $b    = $arr[0]->[2] * $arr[4]->[2] - $arr[5]->[2];
+my $c    = $b - $arr[7]->[2];
+my $step = -$arr[30]->[2];
 
 my $ctr = 0;
 
-for (my $num = 108_100; $num <= 125_100; $num += 17) {
+for (my $num = $b; $num <= $c; $num += $step) {
     $ctr++ unless is_prime($num);
 }
 
 say $ctr;
-
-__END__
-
-while (1) {
-    my @instr = split / /, $arr[$idx];
-    say $idx+1, ": @instr" if $debug;
-    given ($instr[0]) {
-        when (/set/) {
-            $regs{$instr[1]} = $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
-            $idx++;
-        }
-        when (/sub/) {
-            $regs{$instr[1]} -= $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
-            $idx++;
-        }
-        when (/add/) {
-            $regs{$instr[1]} += $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
-            $idx++;
-        }
-        when (/mul/) {
-            $regs{$instr[1]} *= $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
-            $idx++;
-        }
-        when (/mod/) {
-            $regs{$instr[1]} %= $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
-            $idx++;
-        }
-        when (/jnz/) {
-            if ( ( $instr[1] =~ /\d/ ? $instr[1] : $regs{$instr[1]} ) != 0) {
-                $idx += $instr[2] =~ /\d/ ? $instr[2] : $regs{$instr[2]};
-            }
-            else {
-                $idx++;
-            }
-        }
-    }
-    if ($debug) {
-        my @debug = pairwise { $a . ":" . $b } @{[sort keys %regs]}, @{[@regs{qw/a b c d e f g h/}]};
-        say "@debug\n====";
-    }
-    last if ($idx < 0 or $idx > $#arr);
-}
-
-say $regs{h};
