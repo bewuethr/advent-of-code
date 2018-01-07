56ccbba4e303fbc7626d4d167b58f84dde3dd18a
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-01-07 01:45:09 -0500

    Add day 5, part 2, Go

---
+++ 2017/day05/day05b.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := strings.Split(string(input), "\n")
+	// Drop last element (is empty)
+	lines = lines[:len(lines)-1]
+
+	// Convert to integer slice
+	offsets := make([]int, len(lines))
+	for i, v := range lines {
+		offsets[i], err = strconv.Atoi(v)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	ctr := 0
+
+	for idx := 0; idx >= 0 && idx < len(offsets); {
+		next := idx + offsets[idx]
+		if offsets[idx] >= 3 {
+			offsets[idx]--
+		} else {
+			offsets[idx]++
+		}
+		ctr++
+		idx = next
+	}
+
+	fmt.Println(ctr)
+}
