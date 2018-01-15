798850567a14838832764f108d2e077e0628e324
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-01-15 00:02:29 -0500

    Add day 8, part 1, Go

---
+++ 2017/day08/day08a.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var registers map[string]int
+
+func main() {
+	if len(os.Args) == 1 {
+		log.Fatal("Filename not specified")
+	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	registers = make(map[string]int)
+
+	for scanner.Scan() {
+		words := strings.Split(scanner.Text(), " ")
+		reg1, op1, reg2, op2 := words[0], words[1], words[4], words[5]
+		amt1, _ := strconv.Atoi(words[2])
+		amt2, _ := strconv.Atoi(words[6])
+
+		condition := false
+		switch op2 {
+		case "<":
+			condition = registers[reg2] < amt2
+		case "<=":
+			condition = registers[reg2] <= amt2
+		case "==":
+			condition = registers[reg2] == amt2
+		case ">":
+			condition = registers[reg2] > amt2
+		case ">=":
+			condition = registers[reg2] >= amt2
+		case "!=":
+			condition = registers[reg2] != amt2
+		default:
+			log.Fatal("Invalid operator:", op2)
+		}
+
+		var sign int
+		if op1 == "inc" {
+			sign = 1
+		} else {
+			sign = -1
+		}
+
+		if condition {
+			registers[reg1] += sign * amt1
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Reading from file:", err)
+	}
+
+	max := 0
+	for _, v := range registers {
+		if v > max {
+			max = v
+		}
+	}
+
+	fmt.Println(max)
+}
