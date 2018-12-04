8c3789af09797d8adea378b5885fdcd5dd40e6a8
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-12-04 01:23:16 -0500

    Clean up and add command to sort input

--- 2018/day04/day04b.pl
+++ 2018/day04/day04b.pl
@@ -5,14 +5,6 @@ use strict;
 
 use feature 'say';
 
-use List::Util qw(max min reduce sum);
-use List::MoreUtils qw(firstidx firstval pairwise singleton);
-use Algorithm::Combinatorics qw(variations);
-use Math::Prime::Util qw(is_prime);
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
-
-
 my $fname = shift;
 
 open my $fh, "<", $fname
@@ -30,27 +22,19 @@ my ($from, $to);
 while (my $line = <$fh>) {
 	chomp $line;
 	if ($line =~ $re1) {
-		say $line;
-		# say $1;
 		$currentGuard = $1;
 	}
 	if ($line =~ $re2) {
-		say $line;
 		$from = $1;
-		# say $from;
 	}
 	if ($line =~ $re3) {
-		say $line;
 		$to = $1;
-		# say $to;
 		$times->{$currentGuard}{total} += $to - $from;
 		foreach my $minute ($from .. $to-1) {
 			$times->{$currentGuard}{minutes}{$minute}++;
 		}
-		# say Dumper($times);
 	}
 }
-say Dumper($times);
 
 my $max = 0;
 my $bestMinute;
