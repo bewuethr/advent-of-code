07a1f34c5a65c094ea74004e20bd8524dc31b70a
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-12-06 19:02:23 -0500

    Clean up day 6

--- 2018/day06/day06a.pl
+++ 2018/day06/day06a.pl
@@ -5,30 +5,21 @@ use strict;
 
 use feature 'say';
 
-use List::Util qw(max min reduce sum);
-use List::MoreUtils qw(firstidx firstval pairwise singleton);
-use Algorithm::Combinatorics qw(variations);
-use Math::Prime::Util qw(is_prime);
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
+use List::Util qw(max min);
+use List::MoreUtils qw(firstidx);
 
 sub getdist {
 	my ($x, $y, $p) = @_;
-	# say "$x, $y, @$p";
 	return abs($p->[0] - $x) + abs($p->[1] - $y);
 }
 
 sub getnearest {
 	my ($x, $y, $map) = @_;
 	my @distances = map { getdist($x, $y, $_) } @$map;
-	# say "@distances";
 	my $mindistance = min(@distances);
-	# say "min: $mindistance";
-	if ((grep { $_ == $mindistance } @distances) > 1) {
-		# say "-1";
+	if ( (grep { $_ == $mindistance } @distances) > 1 ) {
 		return -1;
 	}
-	# say firstidx { $_ == $mindistance } @distances;
 	return firstidx { $_ == $mindistance } @distances;
 }
 
@@ -45,34 +36,22 @@ while (my $line = <$fh>) {
 	push @map, [$1, $2];
 }
 
-# say Dumper(\@map);
-
 # Bounding box
-my ($xmin, $xmax, $ymin, $ymax) = ($map[0][0], $map[0][0], $map[0][1], $map[0][1]);
-foreach my $coord (@map) {
-	$xmin = $$coord[0] if $$coord[0] < $xmin;
-	$xmax = $$coord[0] if $$coord[0] > $xmax;
-	$ymin = $$coord[1] if $$coord[1] < $ymin;
-	$ymax = $$coord[1] if $$coord[1] > $ymax;
-}
+my ($xmin, $xmax, $ymin, $ymax) = (
+	(min map { $_->[0] } @map),
+	(max map { $_->[0] } @map),
+	(min map { $_->[1] } @map),
+	(max map { $_->[1] } @map),
+);
 
-($xmin, $ymin) = (0, 0);
-$xmax += 100;
-$ymax += 100;
-
-# say "$xmin, $xmax, $ymin, $ymax";
 my %area;
 foreach my $y ($ymin .. $ymax) {
 	foreach my $x ($xmin .. $xmax) {
-		# $nearest[$y][$x] = getnearest($x, $y, \@map);
 		my $val = getnearest($x, $y, \@map);
 		$area{$val}++;
 		print $val == -1 ? "." : chr(65+$val);
-		# say Dumper(\%area);
-		# <>;
 	}
 	say "";
-	# say "@{$nearest[$y]}";
 }
 
 # Find points with infinite areas
@@ -85,8 +64,5 @@ foreach my $y ($ymin .. $ymax) {
 	$infinite{getnearest($xmin, $y, \@map)} //= 1;
 	$infinite{getnearest($xmax, $y, \@map)} //= 1;
 }
-
-say Dumper(\%area);
-say Dumper(\%infinite);
 
 say max( @area{ grep { not exists $infinite{$_} } keys %area } );
