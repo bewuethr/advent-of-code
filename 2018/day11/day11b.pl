ba87c2d979cb4d413e4d65454b28a3f07515299d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-12-11 01:25:32 -0500

    Clean up day 11 part 2

--- 2018/day11/day11b.pl
+++ 2018/day11/day11b.pl
@@ -5,33 +5,18 @@ use strict;
 
 use feature 'say';
 
-use List::Util qw(max min reduce sum);
-use List::MoreUtils qw(firstidx firstval pairwise singleton);
-use Algorithm::Combinatorics qw(variations);
-use Math::Prime::Util qw(is_prime);
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
+use List::Util qw(max sum);
 
 sub getPower {
 	my ($x, $y, $serNum) = @_;
-	my $power = $x + 10;
-	$power *= $y;
-	$power += $serNum;
-	$power *= $x + 10;
+	my $power = (($x + 10) * $y + $serNum) * ($x + 10);
 	$power =~ /(.)..$/;
-	$power = $1;
-	$power //= 0;
-	$power -= 5;
-	return $power;
+	return ($1 // 0) - 5;
 }
 
 sub getPowerBoundary {
 	my ($x, $y, $offset, $powerMap) = @_;
-	# say "x: $x, y: $y, size: $size";
-	my $power = 0;
-	for my $xx ($x .. $x+$offset-1) {
-		$power += $powerMap->[$y+$offset][$xx];
-	}
+	my $power = sum @{ $powerMap->[$y+$offset] }[$x .. $x+$offset-1];
 	for my $yy ($y .. $y+$offset) {
 		$power += $powerMap->[$yy][$x+$offset];
 	}
