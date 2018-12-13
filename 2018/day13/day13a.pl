982c6c355c3bef0eb5fa1c3101d7ae672b3aa4fb
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-12-13 02:30:10 -0500

    Add day 13, as used

---
+++ 2018/day13/day13a.pl
@@ -0,0 +1,144 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use feature 'say';
+
+use List::Util qw(max min reduce sum);
+use List::MoreUtils qw(firstidx firstval pairwise singleton);
+use Algorithm::Combinatorics qw(variations);
+use Math::Prime::Util qw(is_prime);
+use Data::Dumper;
+$Data::Dumper::Sortkeys = 1;
+
+sub draw {
+	my ($map, $carts) = @_;
+	my @drawMap = map { [@$_] } @$map;
+	foreach my $cart (@$carts) {
+		$drawMap[$cart->{y}][$cart->{x}] = "o";
+	}
+	say join "\n", map { join '', @$_ } @drawMap;
+}
+
+sub moveCart {
+	my ($map, $cart) = @_;
+
+	$cart->{x} += $cart->{dx};
+	$cart->{y} += $cart->{dy};
+	# say Dumper($cart);
+	my ($x, $y) = @$cart{ qw(x y) };
+
+	if ($map->[$y][$x] =~ /[-|]/) {
+		return 1;
+	}
+	elsif ($map->[$y][$x] eq "/") {
+		if ($cart->{dy} == 0) {
+			# turn left from horizonal
+			@$cart{ qw(dx dy) } = rotLeft(@$cart{ qw(dx dy) });
+		}
+		else {
+			# turn right from vertical
+			@$cart{ qw(dx dy) } = rotRight(@$cart{ qw(dx dy) });
+		}
+	}
+	elsif ($map->[$y][$x] eq "\\") {
+		if ($cart->{dy} == 0) {
+			# turn right from horizonal
+			@$cart{ qw(dx dy) } = rotRight(@$cart{ qw(dx dy) });
+		}
+		else {
+			# turn left from vertical
+			@$cart{ qw(dx dy) } = rotLeft(@$cart{ qw(dx dy) });
+		}
+	}
+	elsif ($map->[$y][$x] eq "+") {
+		if ($cart->{turn} eq "left") {
+			@$cart{ qw(dx dy) } = rotLeft(@$cart{ qw(dx dy) });
+			$cart->{turn} = "straight";
+		}
+		elsif ($cart->{turn} eq "straight") {
+			$cart->{turn} = "right";
+		}
+		elsif ($cart->{turn} eq "right") {
+			@$cart{ qw(dx dy) } = rotRight(@$cart{ qw(dx dy) });
+			$cart->{turn} = "left";
+		}
+	}
+	else {
+		die "unknown map element at $x/$y: $map->[$y][$x]";
+	}
+	return 1;
+}
+
+sub rotLeft {
+	my ($x, $y) = @_;
+	return ($y, -$x);
+}
+
+sub rotRight {
+	my ($x, $y) = @_;
+	return (-$y, $x);
+}
+
+sub checkCollisions {
+	my ($carts, $idx) = @_;
+	foreach my $i (0 .. $#$carts) {
+		next if $i == $idx;
+		if ($carts->[$i]{x} == $carts->[$idx]{x} and $carts->[$i]{y} == $carts->[$idx]{y}) {
+			say "$carts->[$i]{x},$carts->[$i]{y}";
+			exit
+		}
+	}
+	return 1;
+}
+
+my $fname = shift;
+
+open my $fh, "<", $fname
+    or die "Can't open $fname: $!";
+
+my @map;
+
+# Read map
+while (my $line = <$fh>) {
+	chomp $line;
+	push @map, [ split //, $line ];
+}
+
+my %dirs = (
+	'v' => { dx => 0,  dy => 1 },
+	'^' => { dx => 0,  dy => -1 },
+	'<' => { dx => -1, dy => 0 },
+	'>' => { dx => 1,  dy => 0 },
+);
+
+my @carts;
+
+# Initialize carts and complete map
+foreach my $y (0 .. $#map) {
+	foreach my $x (0 .. $#{$map[0]}) {
+		my $el = $map[$y][$x];
+		if ($el =~ /[<>v^]/) {
+			push @carts, {
+				x    => $x,
+				y    => $y,
+				dx   => $dirs{$el}{dx},
+				dy   => $dirs{$el}{dy},
+				turn => 'left',
+			};
+			$map[$y][$x] =~ tr/<>v^/\-\-||/;
+		}
+	}
+}
+
+# say Dumper \@carts;
+
+while (1) {
+	# draw(\@map, \@carts);
+	@carts = sort { $a->{y} <=> $b->{y} or $a->{x} <=> $b->{x} } @carts;
+	foreach my $idx (0 .. $#carts) {
+		moveCart(\@map, $carts[$idx]);
+		checkCollisions(\@carts, $idx);
+	}
+}
