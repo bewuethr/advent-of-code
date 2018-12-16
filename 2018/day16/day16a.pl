c4be4bb398b5d126b04eec75bccd30f95224d80a
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-12-16 01:18:11 -0500

    Add day 16

---
+++ 2018/day16/day16a.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use feature 'say';
+
+use List::Util qw(max min reduce sum);
+use List::MoreUtils qw(firstidx firstval pairwise singleton);
+use Algorithm::Combinatorics qw(variations);
+use Math::Prime::Util qw(is_prime);
+use Data::Dumper;
+$Data::Dumper::Sortkeys = 1;
+
+sub matches {
+	my ($rb, $i, $ra) = @_;
+	my $count = 0;
+
+	# addr
+	$count++ if $ra->[$i->{C}] == $rb->[$i->{A}] + $rb->[$i->{B}];
+
+	# addi
+	$count++ if $ra->[$i->{C}] == $rb->[$i->{A}] + $i->{B};
+
+	# mulr
+	$count++ if $ra->[$i->{C}] == $rb->[$i->{A}] * $rb->[$i->{B}];
+
+	# muli
+	$count++ if $ra->[$i->{C}] == $rb->[$i->{A}] * $i->{B};
+
+	# banr
+	$count++ if $ra->[$i->{C}] == ($rb->[$i->{A}] & $rb->[$i->{B}]);
+
+	# bani
+	$count++ if $ra->[$i->{C}] == ($rb->[$i->{A}] & $i->{B});
+
+	# borr
+	$count++ if $ra->[$i->{C}] == ($rb->[$i->{A}] | $rb->[$i->{B}]);
+
+	# bori
+	$count++ if $ra->[$i->{C}] == ($rb->[$i->{A}] | $i->{B});
+
+	# setr
+	$count++ if $ra->[$i->{C}] == $rb->[$i->{A}];
+
+	# seti
+	$count++ if $ra->[$i->{C}] == $i->{A};
+
+	# gtir
+	$count++ if $ra->[$i->{C}] == ($i->{A} > $rb->[$i->{B}]);
+
+	# gtri
+	$count++ if $ra->[$i->{C}] == ($rb->[$i->{A}] > $i->{B});
+
+	# gtrr
+	$count++ if $ra->[$i->{C}] == ($rb->[$i->{A}] > $rb->[$i->{B}]);
+
+	# eqir
+	$count++ if $ra->[$i->{C}] == ($i->{A} == $rb->[$i->{B}]);
+
+	# eqri
+	$count++ if $ra->[$i->{C}] == ($rb->[$i->{A}] == $i->{B});
+
+	# eqrr
+	$count++ if $ra->[$i->{C}] == ($rb->[$i->{A}] == $rb->[$i->{B}]);
+
+	return $count;
+}
+
+my $fname = shift;
+
+open my $fh, "<", $fname
+    or die "Can't open $fname: $!";
+
+my $total = 0;
+
+while (1) {
+	my $before = <$fh>;
+	chomp $before;
+	last if $before eq "";
+	my $instr = <$fh>;
+	chomp $instr;
+	my $after = <$fh>;
+	chomp $after;
+	my $blank = <$fh>;
+
+	my (@rb) = $before =~ /(\d+), (\d+), (\d+), (\d)/;
+	my ($opcode, $A, $B, $C) = split / /, $instr;
+	my %i = (
+		op => $opcode,
+		A => $A,
+		B => $B,
+		C => $C,
+	);
+	my (@ra) = $after =~ /(\d+), (\d+), (\d+), (\d)/;
+
+	my $count = matches(\@rb, \%i, \@ra);
+	$total++ if $count >= 3;
+}
+
+say $total;
---
+++ 2018/day16/day16b.pl
@@ -0,0 +1,143 @@
+#!/usr/bin/perl
+
+use 5.010;
+use warnings;
+no warnings 'experimental';
+use strict;
+
+use feature 'say';
+
+use List::Util qw(max min reduce sum);
+use List::MoreUtils qw(firstidx firstval pairwise singleton);
+use Algorithm::Combinatorics qw(variations);
+use Math::Prime::Util qw(is_prime);
+use Data::Dumper;
+$Data::Dumper::Sortkeys = 1;
+
+sub matches {
+	my ($rb, $i, $ra, $cands) = @_;
+
+	# addr
+	$cands->{$i->{op}}{addr} = 1 if $ra->[$i->{C}] == $rb->[$i->{A}] + $rb->[$i->{B}];
+
+	# addi
+	$cands->{$i->{op}}{addi} = 1 if $ra->[$i->{C}] == $rb->[$i->{A}] + $i->{B};
+
+	# mulr
+	$cands->{$i->{op}}{mulr} = 1 if $ra->[$i->{C}] == $rb->[$i->{A}] * $rb->[$i->{B}];
+
+	# muli
+	$cands->{$i->{op}}{muli} = 1 if $ra->[$i->{C}] == $rb->[$i->{A}] * $i->{B};
+
+	# banr
+	$cands->{$i->{op}}{banr} = 1 if $ra->[$i->{C}] == ($rb->[$i->{A}] & $rb->[$i->{B}]);
+
+	# bani
+	$cands->{$i->{op}}{bani} = 1 if $ra->[$i->{C}] == ($rb->[$i->{A}] & $i->{B});
+
+	# borr
+	$cands->{$i->{op}}{borr} = 1 if $ra->[$i->{C}] == ($rb->[$i->{A}] | $rb->[$i->{B}]);
+
+	# bori
+	$cands->{$i->{op}}{bori} = 1 if $ra->[$i->{C}] == ($rb->[$i->{A}] | $i->{B});
+
+	# setr
+	$cands->{$i->{op}}{setr} = 1 if $ra->[$i->{C}] == $rb->[$i->{A}];
+
+	# seti
+	$cands->{$i->{op}}{seti} = 1 if $ra->[$i->{C}] == $i->{A};
+
+	# gtir
+	$cands->{$i->{op}}{gtir} = 1 if $ra->[$i->{C}] == ($i->{A} > $rb->[$i->{B}]);
+
+	# gtri
+	$cands->{$i->{op}}{gtri} = 1 if $ra->[$i->{C}] == ($rb->[$i->{A}] > $i->{B});
+
+	# gtrr
+	$cands->{$i->{op}}{gtrr} = 1 if $ra->[$i->{C}] == ($rb->[$i->{A}] > $rb->[$i->{B}]);
+
+	# eqir
+	$cands->{$i->{op}}{eqir} = 1 if $ra->[$i->{C}] == ($i->{A} == $rb->[$i->{B}]);
+
+	# eqri
+	$cands->{$i->{op}}{eqri} = 1 if $ra->[$i->{C}] == ($rb->[$i->{A}] == $i->{B});
+
+	# eqrr
+	$cands->{$i->{op}}{eqrr} = 1 if $ra->[$i->{C}] == ($rb->[$i->{A}] == $rb->[$i->{B}]);
+}
+
+my $fname = shift;
+
+open my $fh, "<", $fname
+    or die "Can't open $fname: $!";
+
+my %cands;
+
+while (1) {
+	my $before = <$fh>;
+	chomp $before;
+	last if $before eq "";
+	my $instr = <$fh>;
+	chomp $instr;
+	my $after = <$fh>;
+	chomp $after;
+	my $blank = <$fh>;
+
+	my (@rb) = $before =~ /(\d+), (\d+), (\d+), (\d)/;
+	my ($opcode, $A, $B, $C) = split / /, $instr;
+	my %i = (
+		op => $opcode,
+		A => $A,
+		B => $B,
+		C => $C,
+	);
+	my (@ra) = $after =~ /(\d+), (\d+), (\d+), (\d)/;
+
+	matches(\@rb, \%i, \@ra, \%cands);
+}
+
+my %opcodes;
+
+# Find opcodes
+while (1) {
+	last unless scalar keys %cands;
+	my @unambi = grep { my @k = keys %{ $cands{$_} }; @k == 1 } keys %cands;
+	foreach my $opcode (@unambi) {
+		$opcodes{$opcode} = (keys %{ $cands{$opcode} })[0];
+		delete $cands{$opcode};
+		foreach my $key (keys %cands) {
+			delete $cands{$key}{$opcodes{$opcode}};
+		}
+	}
+}
+
+my @r = (0, 0, 0, 0);
+
+# Read and solve program
+while (my $line = <$fh>) {
+	chomp $line;
+	next if $line eq "";
+	my ($opcode, $A, $B, $C) = split / /, $line;
+
+	given ($opcodes{$opcode}) {
+		when('addr') { $r[$C] = $r[$A] + $r[$B] }
+		when('addi') { $r[$C] = $r[$A] + $B }
+		when('mulr') { $r[$C] = $r[$A] * $r[$B] }
+		when('muli') { $r[$C] = $r[$A] * $B }
+		when('banr') { $r[$C] = $r[$A] & $r[$B] }
+		when('bani') { $r[$C] = $r[$A] & $B }
+		when('borr') { $r[$C] = $r[$A] | $r[$B] }
+		when('bori') { $r[$C] = $r[$A] | $B }
+		when('setr') { $r[$C] = $r[$A] }
+		when('seti') { $r[$C] = $A }
+		when('gtir') { $r[$C] = $A > $r[$B] }
+		when('gtri') { $r[$C] = $r[$A] > $B }
+		when('gtrr') { $r[$C] = $r[$A] > $r[$B] }
+		when('eqir') { $r[$C] = $A == $r[$B] }
+		when('eqri') { $r[$C] = $r[$A] == $B }
+		when('eqrr') { $r[$C] = $r[$A] == $r[$B] }
+		default      { die "unknown opcode $opcodes{$opcode}" }
+	}
+}
+
+say $r[0];
