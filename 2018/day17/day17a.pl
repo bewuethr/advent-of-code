8541ec641431db145a431d9b0b617bc83e45875b
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-12-17 23:21:09 -0500

    Clean up day 17

--- 2018/day17/day17a.pl
+++ 2018/day17/day17a.pl
@@ -5,15 +5,9 @@ use strict;
 
 use feature 'say';
 
-use List::Util qw(max min reduce sum);
-use List::MoreUtils qw(firstidx firstval pairwise singleton);
-use Algorithm::Combinatorics qw(variations);
-use Math::Prime::Util qw(is_prime);
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
+use List::Util qw(max min sum);
 
-our $debug = 0;
-our ($xmin, $xmax, $ymin, $ymax);
+our $ymax;
 
 sub draw {
 	my $map = shift;
@@ -22,20 +16,16 @@ sub draw {
 
 sub fall {
 	my ($x, $y, $map) = @_;
-	say "start fall at $x/$y" if $debug;
 	while ($y < $ymax and $map->[$y+1][$x] =~ /[.|]/) {
 		$y++;
 		$map->[$y][$x] = '|';
 	}
-	draw($map) if $debug;
-	<> if $debug;
 	spread($x, $y, $map) unless $y == $ymax;
 }
 
 
 sub spread {
 	my ($xinit, $yinit, $map) = @_;
-	say "start spread at $xinit/$yinit" if $debug;
 	my $pool = 1;
 	my ($x, $y) = ($xinit, $yinit);
 	my ($xmin, $xmax);
@@ -48,8 +38,6 @@ sub spread {
 	if ($map->[$y+1][$x-1] =~ /[.|]/) {
 		$pool = 0;
 		$map->[$y][$x-1] = '|';
-		draw($map) if $debug;
-		<> if $debug;
 		fall($x-1, $y, $map);
 	}
 	else {
@@ -66,8 +54,6 @@ sub spread {
 	if ($map->[$y+1][$x+1] =~ /[.|]/) {
 		$pool = 0;
 		$map->[$y][$x+1] = '|';
-		draw($map) if $debug;
-		<> if $debug;
 		fall($x+1, $y, $map);
 	}
 	else {
@@ -77,8 +63,6 @@ sub spread {
 	if ($pool) {
 		splice(@{$map->[$yinit]}, $xmin, $xmax-$xmin+1, ('~') x ($xmax-$xmin+1));
 	}
-	draw($map) if $debug;
-	<> if $debug;
 }
 
 my $fname = shift;
@@ -115,32 +99,29 @@ while (my $line = <$fh>) {
 	};
 }
 
-$xmin = min keys %clay;
-$xmax = max keys %clay;
-$ymin = min map { keys %$_ } values %clay;
+my $xmin = min keys %clay;
+my $xmax = max keys %clay;
+my $ymin = min map { keys %$_ } values %clay;
 $ymax = max map { keys %$_ } values %clay;
-
-say "$xmin/$ymin to $xmax/$ymax";
 
 my @map;
 
 foreach my $y (0 .. $ymax) {
-	foreach my $x ($xmin-3 .. $xmax+3) {
-		$map[$y][$x-$xmin+3] = exists $clay{$x}{$y} ? '#' : '.';
+	foreach my $x ($xmin-1 .. $xmax+3) {
+		$map[$y][$x-$xmin+1] = exists $clay{$x}{$y} ? '#' : '.';
 	}
 }
 my ($xs, $ys) = (500, 0);
-$map[$ys][$xs-$xmin+3] = '+';
+$map[$ys][$xs-$xmin+1] = '+';
 
-my ($wx, $wy) = ($xs-$xmin+3, $ys);
+my ($wx, $wy) = ($xs-$xmin+1, $ys);
 
+my $oldcount = 0;
 while (1) {
 	fall($wx, $wy, \@map);
-	say Dumper(\@map[0..-1]);
-	say "Total water: ", sum map { scalar grep { $_ =~ /[~|]/ } @$_ } @map[$ymin .. $ymax];
-	# my $newcount = sum map { scalar grep { $_ =~ /[~|]/ } @$_ } @map;
-	# say "Total water: $newcount";
-	# draw(\@map) if $newcount == $oldcount;
-	# last if $newcount == $oldcount;
-	# $oldcount = $newcount;
+	my $newcount = sum map { scalar grep { $_ =~ /[~|]/ } @$_ } @map[$ymin .. $ymax];
+	last if $newcount == $oldcount;
+	$oldcount = $newcount;
 }
+draw(\@map);
+say "Total water: $oldcount";
