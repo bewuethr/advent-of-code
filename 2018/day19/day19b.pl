6940162b23d0b085e5c8d6eeb2668d966fe4348d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-12-19 23:49:03 -0500

    Clean up day 19, part two

--- 2018/day19/day19b.pl
+++ 2018/day19/day19b.pl
@@ -5,16 +5,10 @@ use strict;
 
 use feature 'say';
 
-use List::Util qw(max min reduce sum);
-use List::MoreUtils qw(firstidx firstval pairwise singleton);
-use Algorithm::Combinatorics qw(variations);
-use Math::Prime::Util qw(is_prime);
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
+use List::Util qw(max sum);
+use Math::Prime::Util qw(factor);
 
 our @r;
-
-our $debug = 1;
 
 my %ops = (
 	addr => sub { $r[$_[2]] = $r[$_[0]] + $r[$_[1]] },
@@ -47,23 +41,16 @@ my $ipIdx = (split / /, $ptrLine)[1];
 chomp(my @instr = <$fh>);
 
 @r = (1, 0, 0, 0, 0,  0);
-@r = (0, 10551200, 10551277, 1, 10551200, 4);
-@r = (1, 959200, 10551277, 11, 10551200, 4);
-@r = (12, 10551275, 10551277, 11, 116064025, 4);
-@r = (12, 10, 10551277, 959207, 9592070, 4);
-@r = (959219, 1, 10551277, 10551277, 0, 3);
-@r = (11510496, 10551276, 10551277, 10551277, 0, 3);
 my $ip = $r[$ipIdx];
-$ip = 3;
 
-while (1) {
+# Run fifty operations to find large constant
+foreach my $i (1 .. 50) {
 	my ($opcode, @args) = split / /, $instr[$ip];
 	$r[$ipIdx] = $ip;
-	printf "ip=%2d [" . "%8d, " x 5 . "%8d] $opcode @args ", $ip, @r if $debug;
 	$ops{$opcode}(@args);
-	printf "[" . "%8d, " x 5 . "%8d]\n", @r if $debug;
 	$ip = $r[$ipIdx] + 1;
-	last if $ip > $#instr or $ip < 0;
 }
 
-say $r[0];
+my $n = max @r;
+
+say ((sum factor($n)) + 1 + $n);
