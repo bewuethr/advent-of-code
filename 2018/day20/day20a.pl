d2c675b69ed506ccee7e94e9935a2509e059610d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2019-09-01 22:00:41 -0400

    Add day 20

---
+++ 2018/day20/day20a.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+use v5.10.1;
+use warnings;
+no warnings 'experimental';
+use strict;
+
+use feature 'say';
+
+use List::Util qw(min max);
+
+my %dirs = (
+    N => { dx => 0,  dy => -1 },
+    E => { dx => 1,  dy => 0 },
+    S => { dx => 0,  dy => 1 },
+    W => { dx => -1, dy => 0 },
+);
+
+my $fname = shift;
+
+open my $fh, "<", $fname
+  or die "Can't open $fname: $!";
+
+my $re = <$fh>;
+chomp $re;
+$re = substr( $re, 1, length($re) - 2 );
+
+my @positions;
+my %distances;
+$distances{ 0, 0 } = 0;
+my @cur  = ( 0, 0 );
+my @prev = ( 0, 0 );
+
+foreach my $c ( split //, $re ) {
+    if ( $c eq '(' ) {
+        push @positions, [@cur];
+    }
+    elsif ( $c eq ')' ) {
+        @cur = @{ pop @positions };
+    }
+    elsif ( $c eq '|' ) {
+        @cur = @{ $positions[-1] };
+    }
+    else {
+        $cur[0] += $dirs{$c}{dx};
+        $cur[1] += $dirs{$c}{dy};
+        if ( defined $distances{ $cur[0], $cur[1] } ) {
+            $distances{ $cur[0], $cur[1] } = min(
+                $distances{ $cur[0],  $cur[1] },
+                $distances{ $prev[0], $prev[1] } + 1
+            );
+        }
+        else {
+            $distances{ $cur[0],    $cur[1] } =
+              $distances{ $prev[0], $prev[1] } + 1;
+        }
+    }
+    @prev = @cur;
+}
+
+say max( values %distances );
