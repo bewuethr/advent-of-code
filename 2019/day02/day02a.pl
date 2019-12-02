02f4efeb3326e2fecf1f17aef7b10e8ba67387f5
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2019-12-02 00:26:52 -0500

    Clean up day 2

--- 2019/day02/day02a.pl
+++ 2019/day02/day02a.pl
@@ -5,13 +5,6 @@ use strict;
 
 use feature 'say';
 
-use List::Util qw(first max min reduce sum);
-use List::MoreUtils qw(firstidx firstval pairwise singleton);
-use Algorithm::Combinatorics qw(variations);
-use Math::Prime::Util qw(factor is_prime);
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
-
 my $fname = shift;
 
 open my $fh, "<", $fname
@@ -20,22 +13,21 @@ open my $fh, "<", $fname
 my $line = <$fh>;
 chomp $line;
 my @p = split /,/, $line;
-$p[1] = 12;
-$p[2] = 2;
+@p[ 1, 2 ] = ( 12, 2 );
 
 my $i = 0;
 while (1) {
-	last if $p[$i] == 99;
-	if ($p[$i] == 1) {
-		$p[$p[$i+3]] = $p[$p[$i+1]] + $p[$p[$i+2]];
-	}
-	elsif ($p[$i] == 2) {
-		$p[$p[$i+3]] = $p[$p[$i+1]] * $p[$p[$i+2]];
-	}
-	else {
-		die "illegal opcode at index $i: $p[$i]"
-	}
-	$i += 4;
+    last if $p[$i] == 99;
+    if ( $p[$i] == 1 ) {
+        $p[ $p[ $i + 3 ] ] = $p[ $p[ $i + 1 ] ] + $p[ $p[ $i + 2 ] ];
+    }
+    elsif ( $p[$i] == 2 ) {
+        $p[ $p[ $i + 3 ] ] = $p[ $p[ $i + 1 ] ] * $p[ $p[ $i + 2 ] ];
+    }
+    else {
+        die "illegal opcode at index $i: $p[$i]";
+    }
+    $i += 4;
 }
 
 say $p[0];
--- 2019/day02/day02b.pl
+++ 2019/day02/day02b.pl
@@ -5,13 +5,6 @@ use strict;
 
 use feature 'say';
 
-use List::Util qw(first max min reduce sum);
-use List::MoreUtils qw(firstidx firstval pairwise singleton);
-use Algorithm::Combinatorics qw(variations);
-use Math::Prime::Util qw(factor is_prime);
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
-
 my $fname = shift;
 
 open my $fh, "<", $fname
@@ -23,30 +16,29 @@ my @prog = split /,/, $line;
 
 my $want = 19690720;
 
-foreach my $noun (0..99) {
-	foreach my $verb (0..99) {
-		my @p = @prog;
-		$p[1] = $noun;
-		$p[2] = $verb;
+foreach my $noun ( 0 .. 99 ) {
+    foreach my $verb ( 0 .. 99 ) {
+        my @p = @prog;
+        @p[ 1, 2 ] = ( $noun, $verb );
 
-		my $i = 0;
-		while (1) {
-			last if $p[$i] == 99;
-			if ($p[$i] == 1) {
-				$p[$p[$i+3]] = $p[$p[$i+1]] + $p[$p[$i+2]];
-			}
-			elsif ($p[$i] == 2) {
-				$p[$p[$i+3]] = $p[$p[$i+1]] * $p[$p[$i+2]];
-			}
-			else {
-				die "illegal opcode at index $i: $p[$i]"
-			}
-			$i += 4;
-		}
+        my $i = 0;
+        while (1) {
+            last if $p[$i] == 99;
+            if ( $p[$i] == 1 ) {
+                $p[ $p[ $i + 3 ] ] = $p[ $p[ $i + 1 ] ] + $p[ $p[ $i + 2 ] ];
+            }
+            elsif ( $p[$i] == 2 ) {
+                $p[ $p[ $i + 3 ] ] = $p[ $p[ $i + 1 ] ] * $p[ $p[ $i + 2 ] ];
+            }
+            else {
+                die "illegal opcode at index $i: $p[$i]";
+            }
+            $i += 4;
+        }
 
-		if ($p[0] == $want) {
-			say 100 * $noun + $verb;
-			exit
-		}
-	}
+        if ( $p[0] == $want ) {
+            say 100 * $noun + $verb;
+            last;
+        }
+    }
 }
