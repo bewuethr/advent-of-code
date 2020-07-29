d3596b785a9ad21fbfab5dc2263d7ad9f6fdd3b4
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2020-07-28 23:35:18 -0400

    Update day 3, second part to use grid.Vec2

--- 2019/day03/day03b.go
+++ 2019/day03/day03b.go
@@ -2,42 +2,13 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 
+	"github.com/bewuethr/advent-of-code/go/grid"
 	"github.com/bewuethr/advent-of-code/go/ioutil"
 	"github.com/bewuethr/advent-of-code/go/log"
 )
-
-type point struct {
-	x, y int
-}
-
-type stepPoint struct {
-	point
-	steps int
-}
-
-func (sp *stepPoint) move(dir string, dist int) error {
-	switch dir {
-	case "U":
-		sp.y -= dist
-	case "R":
-		sp.x += dist
-	case "D":
-		sp.y += dist
-	case "L":
-		sp.x -= dist
-	default:
-		return fmt.Errorf("move: illegal direction %v", dir)
-	}
-
-	sp.steps += dist
-	return nil
-}
-
-type path []stepPoint
 
 func main() {
 	scanner, err := ioutil.GetInputScanner()
@@ -55,69 +26,33 @@ func main() {
 		log.Die("reading input", err)
 	}
 
-	path1, err := makePath(wire1)
+	locs1, err := getLocations(wire1)
 	if err != nil {
-		log.Die("making first path", err)
+		log.Die("making first location map", err)
 	}
 
-	path2, err := makePath(wire2)
+	locs2, err := getLocations(wire2)
 	if err != nil {
-		log.Die("making second path", err)
+		log.Die("making second location map", err)
 	}
 
-	intersections := getIntersections(path1, path2)
-	sort.Slice(intersections, func(i, j int) bool {
-		return intersections[i].steps < intersections[j].steps
-	})
-
-	fmt.Println(intersections[0].steps)
+	intersections := getIntersections(locs1, locs2)
+	fmt.Println(getFewestSteps(intersections))
 }
 
-func getIntersections(p1, p2 path) path {
-	pointSet1 := pathToSet(p1[1:])
-	pointSet2 := pathToSet(p2[1:])
-
-	intersections := make(map[point]int)
-
-	for p1, d1 := range pointSet1 {
-		if d2, ok := pointSet2[p1]; ok {
-			intersections[p1] = d1 + d2
-		}
-	}
-
-	var intersectPath path
-	for p, d := range intersections {
-		intersectPath = append(intersectPath, stepPoint{p, d})
-	}
-
-	return intersectPath
+var directions = map[string]grid.Vec2{
+	"U": grid.Uy.ScalarMult(-1),
+	"R": grid.Ux,
+	"D": grid.Uy,
+	"L": grid.Ux.ScalarMult(-1),
 }
 
-func pathToSet(p path) map[point]int {
-	steps := make(map[point]int)
-	for _, pp := range p {
-		if _, ok := steps[pp.point]; !ok {
-			steps[pp.point] = pp.steps
-		}
-	}
-
-	return steps
-}
-
-func contains(points []stepPoint, sp stepPoint) bool {
-	for _, pp := range points {
-		if pp.point == sp.point {
-			return true
-		}
-	}
-
-	return false
-}
-
-func makePath(wireStr []string) (path, error) {
-	var wire path
-	var pos stepPoint
-	wire = append(wire, pos)
+func getLocations(wireStr []string) (map[grid.Vec2]int, error) {
+	var (
+		locations = make(map[grid.Vec2]int)
+		pos       = grid.Origin
+		steps     = 0
+	)
 
 	for _, instr := range wireStr {
 		dir := instr[:1]
@@ -128,36 +63,37 @@ func makePath(wireStr []string) (path, error) {
 		}
 
 		for i := 0; i < dist; i++ {
-			if err := pos.move(dir, 1); err != nil {
-				return nil, err
+			steps++
+			pos = pos.Add(directions[dir])
+			if _, ok := locations[pos]; !ok {
+				locations[pos] = steps
 			}
-			wire = append(wire, pos)
 		}
 	}
 
-	return wire, nil
+	return locations, nil
 }
 
-func manhattanDistance(p point) int {
-	return abs(p.x) + abs(p.y)
-}
+func getIntersections(locs1, locs2 map[grid.Vec2]int) map[grid.Vec2]int {
+	intersections := make(map[grid.Vec2]int)
 
-func abs(n int) int {
-	if n < 0 {
-		return -n
-	}
-	return n
-}
-
-func strSliceToInt(strSlice []string) ([]int, error) {
-	intSlice := make([]int, len(strSlice))
-	for i, s := range strSlice {
-		val, err := strconv.Atoi(s)
-		if err != nil {
-			return nil, err
+	for loc, steps := range locs1 {
+		if _, ok := locs2[loc]; ok {
+			intersections[loc] = steps + locs2[loc]
 		}
-		intSlice[i] = val
 	}
 
-	return intSlice, nil
+	return intersections
+}
+
+func getFewestSteps(vecs map[grid.Vec2]int) int {
+	var fewest int
+
+	for _, s := range vecs {
+		if fewest == 0 || s < fewest {
+			fewest = s
+		}
+	}
+
+	return fewest
 }
