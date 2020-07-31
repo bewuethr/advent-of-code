cb0ae3bd159dd5459d1625726f328aa0cd2504c2
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2020-07-31 01:03:46 -0400

    Add 2019 day 5, second part

---
+++ 2019/day05/second/day05b.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/bewuethr/advent-of-code/go/convert"
+	"github.com/bewuethr/advent-of-code/go/ioutil"
+	"github.com/bewuethr/advent-of-code/go/log"
+)
+
+func main() {
+	scanner, err := ioutil.GetInputScanner()
+	if err != nil {
+		log.Die("getting scanner", err)
+	}
+
+	scanner.Scan()
+	opCodesStr := strings.Split(scanner.Text(), ",")
+	if err := scanner.Err(); err != nil {
+		log.Die("reading input", err)
+	}
+
+	opCodes, err := convert.StrSliceToInt(opCodesStr)
+	if err != nil {
+		log.Die("converting string slice to int", err)
+	}
+
+	if err := runProgram(opCodes); err != nil {
+		log.Die("running op codes", err)
+	}
+}
+
+const (
+	add         = 1
+	mult        = 2
+	input       = 3
+	output      = 4
+	jumpIfTrue  = 5
+	jumpIfFalse = 6
+	lessThan    = 7
+	equals      = 8
+	halt        = 99
+
+	positionMode  = 0
+	immediateMode = 1
+)
+
+var nargs = map[int]int{
+	add:         3,
+	mult:        3,
+	input:       1,
+	output:      1,
+	jumpIfTrue:  2,
+	jumpIfFalse: 2,
+	lessThan:    3,
+	equals:      3,
+	halt:        0,
+}
+
+func runProgram(codes []int) error {
+	idx := 0
+	for {
+		code, modes := parseValue(codes[idx])
+		params := getParams(codes, idx, modes)
+
+		switch code {
+		case halt:
+			return nil
+
+		case add:
+			codes[codes[idx+3]] = params[0] + params[1]
+			idx += nargs[add] + 1
+
+		case mult:
+			codes[codes[idx+3]] = params[0] * params[1]
+			idx += nargs[mult] + 1
+
+		case input:
+			codes[codes[idx+1]] = 5
+			idx += nargs[input] + 1
+
+		case output:
+			fmt.Println(params[0])
+			idx += nargs[output] + 1
+
+		case jumpIfTrue:
+			if params[0] != 0 {
+				idx = params[1]
+			} else {
+				idx += nargs[jumpIfTrue] + 1
+			}
+
+		case jumpIfFalse:
+			if params[0] == 0 {
+				idx = params[1]
+			} else {
+				idx += nargs[jumpIfFalse] + 1
+			}
+
+		case lessThan:
+			if params[0] < params[1] {
+				codes[codes[idx+3]] = 1
+			} else {
+				codes[codes[idx+3]] = 0
+			}
+			idx += nargs[lessThan] + 1
+
+		case equals:
+			if params[0] == params[1] {
+				codes[codes[idx+3]] = 1
+			} else {
+				codes[codes[idx+3]] = 0
+			}
+			idx += nargs[equals] + 1
+
+		default:
+			return fmt.Errorf("illegal opcode %d", codes[idx])
+		}
+	}
+}
+
+func parseValue(val int) (code int, modes []int) {
+	code = val % 100
+	if valStr := strconv.Itoa(val); len(valStr) > 2 {
+		valStr = valStr[:len(valStr)-2]
+
+		var modesStr []string
+		for _, m := range valStr {
+			modesStr = append([]string{string(m)}, modesStr...)
+		}
+
+		var err error
+		modes, err = convert.StrSliceToInt(modesStr)
+		if err != nil {
+			log.Die("converting modes to int", err)
+		}
+	}
+
+	for len(modes) < nargs[code] {
+		modes = append(modes, 0)
+	}
+
+	return code, modes
+}
+
+func getParams(codes []int, idx int, modes []int) []int {
+	var params []int
+
+	for i := 0; i < len(modes); i++ {
+		var param int
+		if modes[i] == immediateMode {
+			param = codes[idx+i+1]
+		} else {
+			param = codes[codes[idx+i+1]]
+		}
+		params = append(params, param)
+	}
+
+	return params
+}
