54edb87e7b22aff293895c42f2e6222feb0a2d71
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2020-09-05 23:34:15 -0400

    Add IntSliceToStr method to convert package

--- go/convert/convert.go
+++ go/convert/convert.go
@@ -17,3 +17,13 @@ func StrSliceToInt(strSlice []string) ([]int, error) {
 
 	return intSlice, nil
 }
+
+// IntSliceToStr converts a slice of ints to strings.
+func IntSliceToStr(intSlice []int) []string {
+	strSlice := make([]string, len(intSlice))
+	for i, n := range intSlice {
+		strSlice[i] = strconv.Itoa(n)
+	}
+
+	return strSlice
+}
