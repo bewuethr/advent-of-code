1bc51b352320f79074d9888518c8bb02ab55cfa1
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2022-01-10 00:11:15 -0500

    Add 2021, day 1, second part

---
+++ 2021/day01/day01b.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import  process from "process";
+import * as fs from "fs";
+
+console.log(fs.readFileSync(process.argv[2], "utf8")
+	.trim()
+	.split("\n")
+	.map(el => Number(el))
+	.map((el, idx, arr) => arr[idx-3] && el > arr[idx-3] ? 1 : 0)
+	.reduce((prev, curr) => prev + curr));
