cb56471945f2e9df9240243c024382b34e964cf0
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2022-01-10 23:50:59 -0500

    Add 2021 day 2, first part

---
+++ 2021/day02/day02a.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import  process from "process";
+import * as fs from "fs";
+
+let pos = 0;
+let depth = 0;
+
+fs.readFileSync(process.argv[2], "utf8")
+	.trim()
+	.split("\n")
+	.map(line => {
+		let [instr, amount] = line.split(" ");
+		return {instr, amount: Number(amount)}
+	})
+	.forEach(({instr, amount}) => {
+		switch(instr) {
+			case "forward":
+				pos += amount;
+				break;
+			case "down":
+				depth += amount;
+				break;
+			case "up":
+				depth -= amount;
+				break;
+			default:
+				console.error(`invalid instruction: ${instr}`);
+		}
+	});
+
+console.log(pos * depth);
