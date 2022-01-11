38d890baec9c103efb0fe7453b9af7d274f685f5
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2022-01-10 19:55:57 -0500

    Simplify map callback

--- 2021/day01/day01a.js
+++ 2021/day01/day01a.js
@@ -6,6 +6,6 @@ import * as fs from "fs";
 console.log(fs.readFileSync(process.argv[2], "utf8")
 	.trim()
 	.split("\n")
-	.map(el => Number(el))
+	.map(Number)
 	.map((el, idx, arr) => arr[idx-1] && el > arr[idx-1] ? 1 : 0)
 	.reduce((prev, curr) => prev + curr));
