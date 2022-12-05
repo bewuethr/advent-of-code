f856a01d09b0079bcf8952a94df5e2f6a10aeffe
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2022-12-05 00:36:37 -0500

    Add 2022, day 5, without reading all input

---
+++ 2022/day05/day05b.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use feature 'say';
+
+my $fname = shift;
+
+my %stacks = (
+    1 => [qw/B W N/],
+    2 => [qw/L Z S P T D M B/],
+    3 => [qw/Q H Z W R/],
+    4 => [qw/W D V J Z R/],
+    5 => [qw/S H M B/],
+    6 => [qw/L G N J H V P B/],
+    7 => [qw/J Q Z F H D L S/],
+    8 => [qw/W S F J G Q B/],
+    9 => [qw/Z W M S C D J/]
+);
+
+open my $fh, "<", $fname
+  or die "Can't open $fname: $!";
+
+while ( my $line = <$fh> ) {
+    chomp $line;
+    next if $line !~ /^move/;
+    my ( $n, $from, $to ) = $line =~ /\d+/g;
+
+    push @{ $stacks{$to} }, splice( @{ $stacks{$from} }, -$n );
+}
+
+my $crates;
+foreach my $i ( 1 .. 9 ) {
+    $crates .= pop @{ $stacks{$i} };
+}
+
+say "$crates";
