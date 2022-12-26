ce64398cf4a1ccb4edc063504f062f2895d313d6
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2022-12-25 19:52:05 -0500

    Add 2022, day 9

---
+++ 2022/day09/day09a.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use feature 'say';
+
+sub add {
+    my ( $visited, $tail ) = @_;
+    $visited->{"$tail->{x}/$tail->{y}"} = 1;
+}
+
+sub move {
+    my ( $head, $dir ) = @_;
+
+    if ( $dir eq "R" ) {
+        ++$head->{x};
+    }
+    elsif ( $dir eq "L" ) {
+        --$head->{x};
+    }
+    elsif ( $dir eq "U" ) {
+        --$head->{y};
+    }
+    elsif ( $dir eq "D" ) {
+        ++$head->{y};
+    }
+    else {
+        die("invalid direction $dir");
+    }
+}
+
+sub follow {
+    my ( $head, $tail ) = @_;
+
+    if ( $head->{y} eq $tail->{y} ) {
+        if ( $head->{x} == $tail->{x} + 2 ) {
+            ++$tail->{x};
+        }
+        elsif ( $head->{x} == $tail->{x} - 2 ) {
+            --$tail->{x};
+        }
+    }
+    elsif ( $head->{x} eq $tail->{x} ) {
+        if ( $head->{y} == $tail->{y} + 2 ) {
+            ++$tail->{y};
+        }
+        elsif ( $head->{y} == $tail->{y} - 2 ) {
+            --$tail->{y};
+        }
+    }
+    elsif (
+        abs( $head->{x} - $tail->{x} ) + abs( $head->{y} - $tail->{y} ) > 2 )
+    {
+        $tail->{x} += $head->{x} > $tail->{x} ? 1 : -1;
+        $tail->{y} += $head->{y} > $tail->{y} ? 1 : -1;
+    }
+}
+
+my $fname = shift;
+
+open my $fh, "<", $fname
+  or die "Can't open $fname: $!";
+
+my $head = { x => 0, y => 0 };
+my $tail = { x => 0, y => 0 };
+
+my $visited = {};
+
+add( $visited, $tail );
+
+while ( my $line = <$fh> ) {
+    chomp $line;
+    my ( $dir, $n ) = split / /, $line;
+
+    while ( $n-- ) {
+        move( $head, $dir );
+        follow( $head, $tail );
+        add( $visited, $tail );
+    }
+}
+
+say scalar keys %$visited;
